ad8a74e81c0ea3f954eb0b5dc0fcb15d36011e63
Frobi <frobi.webdev@outlook.com>
2022-04-04 15:41:31 +0930

    create-game-over-statement-with-5-win-or-loss-trigger

--- script.js
+++ script.js
@@ -42,31 +42,41 @@ function playRound(playerSelection) {
 
 let winCounter = 0;
 let lossCounter = 0;
+let gameOver = "";
 
 function game() {
-    playRound(playerSelection); {
-        if (outcome == 
-            "You win! Scissors beats Paper" ||
-            outcome == 
-            "You win! Paper Beats rock!" ||
-            outcome == 
-            "You win! Rock beats scissors!" ) 
-            { winCounter++;
-                // setup a counter that goes up by one every time the player wins
-        } else if 
-            (outcome === 
-            "You lose! Paper beats rock!" ||
-            outcome ==
-            "You lose! Scissors beats paper!" ||
-            outcome ==
-            "You lose! Rock beats scissors" )
-            { lossCounter++;
-                // setup a counter that goes up by one every time the player loses
-        } else 
-            (outcome ===
-            "Draw!");
-    return outcome
+    if (winCounter >= 5 && lossCounter < 5) {
+        gameOver = "You were the first to 5 wins! Conratulations!"
     };
-    
+     // create game over statement for a win
+
+    if (lossCounter >= 5 && winCounter < 5) {
+        gameOver = "The computer beat you to 5 wins! Better luck next time!"
+    };
+     // create game over statement for a loss
+
+        playRound(playerSelection); {
+            if (outcome == 
+                "You win! Scissors beats Paper" ||
+                outcome == 
+                "You win! Paper Beats rock!" ||
+                outcome == 
+                "You win! Rock beats scissors!" ) 
+                { winCounter++;
+                    // setup a counter that goes up by one every time the player wins
+            } else if 
+                (outcome === 
+                "You lose! Paper beats rock!" ||
+                outcome ==
+                "You lose! Scissors beats paper!" ||
+                outcome ==
+                "You lose! Rock beats scissors" )
+                { lossCounter++;
+                    // setup a counter that goes up by one every time the player loses
+            } else 
+                (outcome ===
+                "Draw!");
+        return outcome
+    };
 };
 
